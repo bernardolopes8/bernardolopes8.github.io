5acca60eef686756a742e03e1292aa2495795f19
Bernardo Lopes <bernardo.rzr@gmail.com>
2018-08-11 17:11:12 +0100

    Add typed.js

--- js/creative.js
+++ js/creative.js
@@ -1,6 +1,15 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  // Typed.js
+   var typed = new Typed("#typed", {
+    stringsElement: '#typed-strings',
+    typeSpeed: 43,
+    backSpeed: 15,
+    backDelay: 1000,
+    loop: true,
+  });
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
